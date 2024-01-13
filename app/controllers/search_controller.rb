4e4bb228417f2c955f7c6a51c57543388a089bde
jic <ji_camargo@yahoo.com>
2024-01-12 20:19:45 -0500

    add basic logic to controller and index view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,2 +1,9 @@
 class SearchController < ApplicationController
+  def index
+    @search_query = params[:query]
+    @ip_adrress = request.remote_ip
+    #user field is created for future use
+    @user ="Guest" 
+    SearchStory.create(query: @search_query, ip_address: @ip_adrress, user: @user)
+  end
 end
