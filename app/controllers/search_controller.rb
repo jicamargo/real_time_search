5c36707a05acd1a6107e99fa06fca7918b3c766f
jic <ji_camargo@yahoo.com>
2024-01-13 00:13:21 -0500

    start seaarch optimization

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,5 @@
 class SearchController < ApplicationController
   def index
-    @search_query = params[:query]
-    @ip_adrress = request.remote_ip
-    #user field is created for future use
-    @user ="Guest" 
-
-    SearchStory.create(query: @search_query, ip_address: @ip_adrress, user: @user)
-
-    # @search_results = SearchStory.where("query ILIKE ?", "%#{@search_query}%").pluck(:query)
-    # puts @search_results
 
     respond_to do |format|
       format.html { render :index }
@@ -17,6 +8,12 @@ class SearchController < ApplicationController
   end
 
   def get_queries
+    @search_query = params[:query]
+    @ip_address = request.remote_ip
+    #user field is created for future use
+    @user ="Guest" 
+    SearchStory.create(query: @search_query, ip_address: @ip_address, user: @user)
+
     @search_query = params[:query]
     @search_results = SearchStory.where("query ILIKE ?", "%#{@search_query}%").pluck(:query)
     puts @search_results
--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -11,27 +11,39 @@ export default class extends Controller {
   search() {
     const searchResults = this.resultListTarget;
     const query = this.inputTarget.value.trim();
+    const key = this.inputTarget.value.slice(-1);
+    let currentQuery = '';
 
-    // Calls the controller action /search/index with the query parameter
-    fetch(`/search/get_queries?query=${query}`)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`Network response error: ${response.status}`);
-        }
-        return response.json();
+    console.log('key: ' + key);
+    if (key === ' ' || key === '.' || key === ',' || this.inputType === 'insertCompositionText') {
+
+      // Calls the controller action /search/index with the query parameter
+      fetch(`/search/get_queries?query=${query}`)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`Network response error: ${response.status}`);
+          }
+          return response.json();
+        })
+        .then(results => {
+          // Fisrt clear all and Add results to list
+          searchResults.innerHTML = '';
+
+          if (Array.isArray(results)) {
+            results.forEach(result => {
+              const listItem = document.createElement('li');
+              listItem.textContent = result;
+              searchResults.appendChild(listItem);
+            });
+          }
       })
-      .then(results => {
-        // Fisrt clear all and Add results to list
-        searchResults.innerHTML = '';
+      .catch(error => console.error('Error getting queries:', error));
 
-        if (Array.isArray(results)) {
-          results.forEach(result => {
-            const listItem = document.createElement('li');
-            listItem.textContent = result;
-            searchResults.appendChild(listItem);
-          });
-        }
-    })
-    .catch(error => console.error('Error getting queries:', error));
+      currentQuery = '';
+    }
+    else
+    {
+      currentQuery += key;
+    }
   }
 }
